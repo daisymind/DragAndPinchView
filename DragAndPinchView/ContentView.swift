eb4acb1fb5c58e72ef2e4a76c57887c6ccd9b3b9
ShinTotoki <shin1010ki@icloud.com>
2022-10-06 23:14:51 +0900

    Initial Commit

---
+++ DragAndPinchView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DragAndPinchView
+//
+//  Created by Shin on 2022/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DragAndPinchView/DragAndPinchViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndPinchViewApp.swift
+//  DragAndPinchView
+//
+//  Created by Shin on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct DragAndPinchViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
